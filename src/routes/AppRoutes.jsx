5a2a6e6016abba25b9128e664ae9cb25b902e416
DayaneNogueira <dayane.nogueira@alunos.sc.senac.br>
2024-06-06 21:47:56 -0300

    ajuste react código

---
+++ src/routes/AppRoutes.jsx
@@ -0,0 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Theme from './pages/Theme';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="theme/*" element={<Theme />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
