bbd05f4a660764e5b0c28629e0fea857ac5c4c05
DayaneNogueira <dayane.nogueira@alunos.sc.senac.br>
2024-07-08 22:27:45 -0300

    ajuste react

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,10 +7,13 @@ export default function Footer() {
             <div className={style.brand}>
                 <img src={icon} alt="Personal Development soon" height={100} width={100}/>
             </div>
+            <section className={style.siteMapLinks}>
                 <h2>Links Úteis</h2>
                 <ul>
                     <li><a className={style.link} href="/">Início</a></li>
+                    <li><a className={style.link} href="https://github.com/DayaneNogueira/reactPersonalDevelopment.git">Código Fonte</a></li>
                 </ul>
+            </section>
         </footer>
     )
 }
